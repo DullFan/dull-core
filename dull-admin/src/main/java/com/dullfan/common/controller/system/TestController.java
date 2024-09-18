f712468709a22d7837cdc7922c80c34ade5b1294
DullFan <2928527233@qq.com>
2024-09-18 19:20:54 +0800

    测试Jenkins

--- dull-admin/src/main/java/com/dullfan/common/controller/system/TestController.java
+++ dull-admin/src/main/java/com/dullfan/common/controller/system/TestController.java
@@ -15,6 +15,6 @@ public class TestController extends ABaseController {
 
     @GetMapping("/getInfo")
     public Result login() {
-        return success("测试文件");
+        return success("更新测试文件");
     }
 }
